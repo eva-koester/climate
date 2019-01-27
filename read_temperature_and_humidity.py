257cefade49a11d7b5824e1ec04203c6ef9f6b76
eva-koester <eva.maria.koester@gmx.de>
2019-01-27 12:21:02 +0000

    reading temperature and humidity

---
+++ read_temperature_and_humidity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
+
+sensor=11
+pin=4
+
+while True:
+    humidity, temperature=Adafruit_DHT.read_retry(sensor, pin)
+    print('Temp:{0:0.1f} C Humidity:{1:0.1f}'.format(temperature,humidity))
+
