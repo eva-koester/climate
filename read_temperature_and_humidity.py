35f7ebf262f96a93a47d04bdec180f2aed1a3d95
eva-koester <eva.maria.koester@gmx.de>
2019-05-03 15:28:34 +0100

    Log to CSV file

--- read_temperature_and_humidity.py
+++ read_temperature_and_humidity.py
@@ -9,3 +9,6 @@ while True:
     humidity, temperature=Adafruit_DHT.read_retry(sensor, pin)
     print('Temp:{0:0.1f} C Humidity:{1:0.1f}'.format(temperature,humidity))
 
+    with open("log.csv", "at") as logfile:
+        logfile.write("{0:0.2f},{1:0.2f}\n".format(temperature, humidity))
+
